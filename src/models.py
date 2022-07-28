5f4e040f374c18271c1f039e6a707b5eb44f4c70
Samuel Palacios <samuelpl0888@gmail.com>
2022-07-28 14:48:16 -0400

    Update models.py

--- src/models.py
+++ src/models.py
@@ -8,7 +8,7 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True,nullable=False)
-    password = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
     # articles = db.relationship('Article', backref='user')
 
     def serialize(self):
