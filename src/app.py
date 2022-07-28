009ddc5c7f83f38a90f564f4cde8d26fea1c811f
Samuel Palacios <samuelpl0888@gmail.com>
2022-07-28 14:25:16 -0400

    API Working

--- src/app.py
+++ src/app.py
@@ -1,16 +1,40 @@
 from flask import Flask, jsonify, request
 from flask_migrate import Migrate
-from models import db
+from models import db, Article
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['ENV'] = 'development'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///starwars.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 db.init_app(app)
 Migrate(app, db) # db init, db migrate, db upgrade
 
 #### code inside this lines ####
+
+@app.route('/api/articles', methods=['GET', 'POST']) 
+def list_and_create_post():
+    # returns all articles
+    if request.method == ('GET'):
+        articles = Article.query.all()
+        articles = list(map(lambda article: article.serialize(), articles))
+
+        return jsonify(articles), 200 
+
+    # function to create post
+    if request.method == ('POST'):
+        data = request.get_json()
+
+        article = Article()
+        article.title = data['title']
+        article.body = data['body']
+        article.created_by = data['created_by']
+
+        db.session.add(article)
+        db.session.commit()
+
+        return jsonify(article.serialize()), 201
+
 
 #### code inside this lines ####
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,2 +1,36 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+from sqlalchemy import ForeignKey
 db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True,nullable=False)
+    password = db.Column(db.String(100), unique=True, nullable=False)
+    # articles = db.relationship('Article', backref='user')
+
+    def serialize(self):
+        return{
+            'id': self.id,
+            'username': self.username,
+        }
+
+class Article(db.Model):
+    __tablename__ = 'articles'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    body = db.Column(db.Text(), nullable=False)
+    date_posted = db.Column(db.DateTime(), default=datetime.now())
+    created_by = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user = db.relationship('User')
+
+    def serialize(self):
+        return{
+            'id': self.id,
+            'title': self.title,
+            'body': self.body,
+            'date_posted': self.date_posted,
+            'user': self.user.username
+        }
